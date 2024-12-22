bb5e47c15d9b559a46101ddeabf65d435a3e41a2
Dana2611 <dana.margineanu3@yahoo.com>
2024-12-22 14:03:34 +0200

    Add project files.

---
+++ Margineanu_Dana_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Margineanu_Dana_Lab11.Data;
+
+namespace Margineanu_Dana_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Margineanu_Dana_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Margineanu_Dana_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
